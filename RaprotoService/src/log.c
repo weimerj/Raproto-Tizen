4e4073e838c530577abce95de2bd331f3cd0f6ff
James Weimer <james.erich.weimer@gmail.com>
2021-02-23 15:32:25 -0500

    settings

--- RaprotoService/src/log.c
+++ RaprotoService/src/log.c
@@ -267,8 +267,14 @@ log_sensor_setup(int idx, sensor_type_e type, const char *key, sensor_event_cb c
 			if ((err = sensor_create_listener(ad->sensors[idx].sensor, &(ad->sensors[idx].listener))) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "sensor_create_listener");
 			if ((err = sensor_listener_set_event_cb(ad->sensors[idx].listener, pos_log, callback, ad)) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "sensor_listener_set_event_cb");
 			if ((err = sensor_listener_set_option(ad->sensors[idx].listener, SENSOR_OPTION_ALWAYS_ON)) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "sensor_listener_set_option");
+
+
 			if ((err = sensor_listener_set_attribute_int(ad->sensors[idx].listener, SENSOR_ATTRIBUTE_AXIS_ORIENTATION, SENSOR_AXIS_DEVICE_ORIENTED)) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "sensor_listener_set_attribute_int 1");
 			if ((err = sensor_listener_set_attribute_int(ad->sensors[idx].listener, SENSOR_ATTRIBUTE_PAUSE_POLICY, SENSOR_PAUSE_NONE)) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "sensor_listener_set_attribute_int 2");
+
+
+
+
 			if ((err = sensor_listener_start(ad->sensors[idx].listener)) != SENSOR_ERROR_NONE) return error_msg(err, __func__, "listener start");
 
 
